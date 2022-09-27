db1c711d66bb0ac25d94f4a84c7195e8eaccbae2
anthony-derose <anthony.derose@rutgers.edu>
2022-09-27 03:52:43 -0400

    db init

--- init_db.py
+++ init_db.py
@@ -2,7 +2,7 @@ import os
 import psycopg2
 
 conn = psycopg2.connect(
-        host="localhost",
+        host="https://receipterboxd.herokuapp.com",
         database="d7gro9lm48k1ee",
         user='qvurpggflbstyh',
         password='7cd16cffef1204303b0250d7a28d45074dea93c0371dcf37684771fc3d796f0d',
@@ -14,7 +14,7 @@ cur = conn.cursor()
 # Execute a command: this creates a new table
 cur.execute('DROP TABLE IF EXISTS users;')
 cur.execute('CREATE TABLE users (id serial PRIMARY KEY,'
-                                 'username varchar (50) UNIQUE NOT NULL);'
+                                'username varchar (50) UNIQUE NOT NULL);'
                                 )
 
 # Insert data into the table
