9a930ea5b9bfd421b08b2abd26060c232d00c0c7
anthony-derose <anthony.derose@rutgers.edu>
2022-09-28 01:38:45 -0400

    fixed connection for user

--- api/user.py
+++ api/user.py
@@ -5,10 +5,11 @@ import psycopg2
 
 def get_db_connection():
   conn = psycopg2.connect(
-          host="localhost",
-          database="my_db",
-          user='anthony',
-          password='antoine')
+          host="ec2-44-207-253-50.compute-1.amazonaws.com",
+          database="d7gro9lm48k1ee",
+          user='qvurpggflbstyh',
+          password='7cd16cffef1204303b0250d7a28d45074dea93c0371dcf37684771fc3d796f0d',
+          port='5432') 
   return conn
 
 class User(Resource):
